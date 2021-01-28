b301c2bd6189af840575d19a265779ee12a3a080
johkl473 <johkl473@student.liu.se>
2021-01-28 11:24:24 +0100

    La till vänterutiner, look-uptabell för 7-Seg och två hårdkodade TWI_SEND för testning

--- Lab1_TWI/Lab1_TWI/main.asm
+++ Lab1_TWI/Lab1_TWI/main.asm
@@ -8,33 +8,43 @@
 ;
 ;::::::::::::::::::::::::::::::::::::::::::::
 
+
+;::::::::::::::::
 ; Övergripande beskrivning av programmet för dokumentation
+; Fyll på här eftersom
 ;
-;
+;::::::::::::::::
+
+		jmp		COLD
 
 ;::::::::::::::::
 ;	Data
 ;::::::::::::::::
+		
+		; Här kan vi köra satta värden som .equ listor och liknande
 
-		.equ	ADDR_RIGHT8	= $25						; Här kan vi köra satta värden som .equ listor och liknande
-		.equ	SLA_W		= (ADDR_RIGHT8 << 1) | 0
-		.equ	SLA_R		= (ADDR_RIGHT8 << 1) | 1
+		.equ	ADDR_RIGHT8	= $25						
+		.equ	SLA_W		= (ADDR_RIGHT8 << 1) | 0	; $4A 0b01001010
+		.equ	SLA_R		= (ADDR_RIGHT8 << 1) | 1	; $4B 0b01001011
 
 		.equ	SCL		= PC5
 		.equ	SDA		= PC4
 
-				MISO
-				MOSI
-				SCK
-				CS_Modul1
-				CS_Modul2
+
+		.equ	N		= $64							; Styr en sekund delay DELAY_N, som går att variera lite
+														; $64 = ~1000,03 ms om DELAY=10ms
+														; $3D = ~999,5 ms om DELAY=16ms
+
+7SEG:
+		.db 	$3F, $06, $5B, $4F, $66, $6D, $7D, $07, $7F, $6F, $77, $7C, $39, $5E, $79, $71
+														; LOOKUP-tabell för 0-F i 7-seg (pgfedcba)
 ;::::::::::::::::
 ;	Uppstart
 ;::::::::::::::::
 
 
 COLD:
-										; Initiera stackpekaren
+														; Initiera stackpekaren
 		ldi 	r16, HIGH(RAMEND)
 		out 	SPH, r16
 		ldi 	r16, LOW(RAMEND)
@@ -47,7 +57,10 @@ COLD:
 
 
 MAIN:
-		call	WAIT_ALT
+		call	DELAY_N
+		call	HARD_0_TWI_SEND
+		call	DELAY_N
+		call	HARD_7_TWI_SEND
 		rjmp	MAIN
 
 
@@ -55,17 +68,74 @@ MAIN:
 ;	Subrutiner
 ;::::::::::::::::
 
+HARD_0_TWI_SEND:
+		call	START
+									; Adress $4A 0b01001010
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+
+		call	SDH		; ACK
+									; Data $3F 0b00111111
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDH					; 1
+		call	SDH					; 1
+		call	SDH					; 1
+		call	SDH					; 1
+		call	SDH					; 1
+
+		call	SDH		; ACK
+
+		call	STOP
+		ret
+
+	; ---------------
+
+HARD_7_TWI_SEND:
+		call	START
+									; Adress $4A 0b01001010
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDL					; 0
+
+		call	SDH		; ACK
+									; Data $07 0b00000111
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDL					; 0
+		call	SDH					; 1
+		call	SDH					; 1
+		call	SDH					; 1
+
+		call	SDH		; ACK
+
+		call	STOP
+		ret
+
+	; ---------------
+
 TWI_SEND:
 		call	START
-		call	SEND_ADR			; +R/W'
+		;call	SEND_ADR			; +R/W'
 									; Släpp SDA för att lyssna på ACK + 1 CP
-		call	SEND_DATA
+		;call	SEND_DATA
 		call	STOP
 
-
-
-
-
+	; ---------------
 
 START:
 		sbi		DDRC, SDA
@@ -73,6 +143,8 @@ START:
 		sbi		DDRC, SCL
 		call	WAIT
 		ret
+
+	; ---------------
 
 STOP:
 		sbi		DDRC, SDA
@@ -83,6 +155,8 @@ STOP:
 		call	WAIT
 		ret
 
+	; ---------------
+
 SDL:
 		sbi		DDRC, SDA
 		call	WAIT
@@ -91,6 +165,8 @@ SDL:
 		sbi		DDRC, SCL
 		call	WAIT
 		ret
+
+	; ---------------
 
 SDH:
 		cbi		DDRC, SDA
@@ -101,22 +177,38 @@ SDH:
 		call	WAIT
 		ret
 
-/*WAIT:						
-							; Vänte-loop, upp till ~16 ms
-		push	r25
-		push	r24
-		ldi 	r25, $63	; $63C4 ger 160000 cykler för hela rutinen, i princip exakt 10.0 ms, $FE6F ~100 µs
-		ldi 	r24, $C4	
-W1:
-		adiw	r24, 1
-		brne	W1
-		pop		r24
-		pop		r25
-		ret	*/
+;::	Vänterutiner ::
 
 WAIT:
 		ldi		r16, $34
 W1:
 		dec		r16
 		brne	W1
+		ret
+
+	; ---------------
+
+DELAY_N:						; Längre vänteloop, styrt av N som är definerat i början under "Data".
+		push	r16
+		ldi		r16, N
+DELAY_N1:
+		call	DELAY
+		dec 	r16
+		brne	DELAY_N1
+		pop		r16
+		ret
+
+	; ---------------
+
+DELAY:						
+								; Vänte-loop, upp till ~16 ms ($FFFF, här 10 ms
+		push	r25
+		push	r24
+		ldi 	r25, $63		; $63C4 ger 160000 cykler för hela rutinen, i princip exakt 10.0 ms
+		ldi 	r24, $C4	
+D1:
+		adiw	r24, 1
+		brne	D1
+		pop		r24
+		pop		r25
 		ret
